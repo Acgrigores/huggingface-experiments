5fd6bc0492bc51b804bf1e5ce7076d3667881ddf
Anna Grigorescu <77759059+Acgrigores@users.noreply.github.com>
2022-06-07 15:29:02 -0500

    Update commandLine.bash

--- huggingface_experiments/commandLine.bash
+++ huggingface_experiments/commandLine.bash
@@ -1,2 +1,8 @@
 #!/bin/bash
+#Login
 huggingface-cli login
+#Create and clone new repo outside of organization
+huggingface-cli create REPONAME -y
+git clone https://huggingface.co/USERNAME/REPONAME
+#Create and clone new repo within organization
+huggingface-cli repo create --organization SoftwareEngineering-Test NAME
